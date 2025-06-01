11e68b2bce68cb9dabdc3a6f79a7c72ea3405e7e
Андрей Лукашук <andriy2006lukashuk@gmail.com>
2025-06-02 00:57:17 +0200

    add all components

--- src/mtsAPI.js
+++ src/mtsAPI.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
-const API_KEY = '3d55676118a80418b3bbc38dfa2ecc22'; 
+const API_KEY = '3d55676118a80418b3bbc38dfa2ecc22';
 const BASE_URL = 'https://api.themoviedb.org/3';
-export const IMAGE_BASE_URL = 'https://image.tmdb.org/t/p/w500';
+const IMAGE_BASE_URL = 'https://image.tmdb.org/t/p/w500';
 
 const options = {
   params: {
@@ -10,54 +10,37 @@ const options = {
   },
 };
 
+export { IMAGE_BASE_URL };
+
 export async function fetchTrendingMovies() {
-  const response = await axios.get(`${BASE_URL}/trending/movie/day`, options);
-  return response.data.results;
+  const resp = await axios.get(`${BASE_URL}/trending/movie/day`, options);
+  return resp.data.results;
 }
 
 export async function fetchMovieDetails(movieId) {
-  const response = await axios.get(`${BASE_URL}/movie/${movieId}`, {
-    ...options,
-    params: {
-      ...options.params,
-      language: 'en-US',
-    },
-  });
-  return response.data;
+  const resp = await axios.get(`${BASE_URL}/movie/${movieId}`, options);
+  return resp.data;
 }
 
 export async function fetchMovieCast(movieId) {
-  const response = await axios.get(`${BASE_URL}/movie/${movieId}/credits`, {
-    ...options,
-    params: {
-      ...options.params,
-      language: 'en-US',
-    },
-  });
-  return response.data.cast;
+  const resp = await axios.get(`${BASE_URL}/movie/${movieId}/credits`, options);
+  return resp.data.cast;
 }
 
 export async function fetchMovieReviews(movieId) {
-  const response = await axios.get(`${BASE_URL}/movie/${movieId}/reviews`, {
-    ...options,
-    params: {
-      ...options.params,
-      language: 'en-US',
-    },
-  });
-  return response.data.results;
+  const resp = await axios.get(`${BASE_URL}/movie/${movieId}/reviews`, options);
+  return resp.data.results;
 }
 
 export async function fetchMovies(query) {
-  const response = await axios.get(`${BASE_URL}/search/movie`, {
-    ...options,
+  const resp = await axios.get(`${BASE_URL}/search/movie`, {
     params: {
-      ...options.params,
-      query: query,
+      api_key: API_KEY,
+      query,
       include_adult: false,
       language: 'en-US',
       page: 1,
     },
   });
-  return response.data.results;
+  return resp.data.results;
 }
