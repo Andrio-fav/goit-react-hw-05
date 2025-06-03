659d994feaf7a85eb65dfcb0d470e38f19c0a21c
Андрей Лукашук <andriy2006lukashuk@gmail.com>
2025-06-03 23:11:02 +0200

    add MovieList

--- src/components/MovieList/MovieList.jsx
+++ src/components/MovieList/MovieList.jsx
@@ -3,23 +3,16 @@ import { Link, useLocation } from 'react-router-dom';
 
 export default function MovieList({ movies }) {
   const location = useLocation();
-  console.log(location);
 
   return (
-    <ul className={styles.movieList}>
-      {movies.map((movie) => {
-        return (
-          <li key={movie.id} className={styles.movieItem}>
-            <Link
-              to={`${movie.id}`}
-              className={styles.movieLink}
-              state={location}
-            >
-              {movie.title}
-            </Link>
-          </li>
-        );
-      })}
+    <ul className={styles.movielist}>
+      {movies.map(movie => (
+        <li key={movie.id} className={styles.movieItem}>
+          <Link to={`/movies/${movie.id}`} state={{ from: location }}>
+            {movie.title || movie.name}
+          </Link>
+        </li>
+      ))}
     </ul>
   );
 }
